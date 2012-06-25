162e563914fa2a1c8a8244b341620d5de2616803
Torsten Zesch <zesch@users.noreply.github.com>
2012-06-25 08:19:45 +0000

    Refactored resources a bit.

--- de.tudarmstadt.ukp.similarity.dkpro.core-asl/src/main/java/de/tudarmstadt/ukp/similarity/dkpro/resource/lexical/WordNGramContainmentResource.java
+++ de.tudarmstadt.ukp.similarity.dkpro.core-asl/src/main/java/de/tudarmstadt/ukp/similarity/dkpro/resource/lexical/WordNGramContainmentResource.java
@@ -1,29 +1,23 @@
 package de.tudarmstadt.ukp.similarity.dkpro.resource.lexical;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.Map;
 
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.resource.ResourceSpecifier;
 import org.uimafit.descriptor.ConfigurationParameter;
 
-import de.tudarmstadt.ukp.similarity.algorithms.lexical.ngrams.CharacterNGramMeasure;
+import de.tudarmstadt.ukp.similarity.algorithms.lexical.ngrams.WordNGramContainmentMeasure;
 import de.tudarmstadt.ukp.similarity.dkpro.resource.TextSimilarityResourceBase;
 
 
-public class CharacterNGramResource
+public class WordNGramContainmentResource
 	extends TextSimilarityResourceBase
 {
 	public static final String PARAM_N = "N";
 	@ConfigurationParameter(name=PARAM_N, mandatory=true)
 	private int n;
 	
-	public static final String PARAM_IDF_VALUES_FILE = "IdfValuesFile";
-	@ConfigurationParameter(name=PARAM_IDF_VALUES_FILE, mandatory=true)
-	private File idfValuesFile;
-	
-	@SuppressWarnings("unchecked")
+    @SuppressWarnings("unchecked")
     @Override
     public boolean initialize(ResourceSpecifier aSpecifier, Map aAdditionalParams)
         throws ResourceInitializationException
@@ -31,13 +25,8 @@ public class CharacterNGramResource
         if (!super.initialize(aSpecifier, aAdditionalParams)) {
             return false;
         }
-
-        try {
-            measure = new CharacterNGramMeasure(n, idfValuesFile);
-        }
-        catch (IOException e) {
-            throw new ResourceInitializationException(e);
-        }
+        
+		measure = new WordNGramContainmentMeasure(n);
         
         return true;
     }
