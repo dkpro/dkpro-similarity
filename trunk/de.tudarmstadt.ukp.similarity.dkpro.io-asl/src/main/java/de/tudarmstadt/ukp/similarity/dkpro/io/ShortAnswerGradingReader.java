06be90eec9f3510243a9933bb7746a13138e7bd2
torsten.zesch <torsten.zesch@ac01e6db-ab4e-4c12-cdae-32caa6b7cbd2>
2012-06-28 16:41:10 +0000

    Now also writes an annotation containing the gold standard value.

--- trunk/de.tudarmstadt.ukp.similarity.dkpro.io-asl/src/main/java/de/tudarmstadt/ukp/similarity/dkpro/io/ShortAnswerGradingReader.java
+++ trunk/de.tudarmstadt.ukp.similarity.dkpro.io-asl/src/main/java/de/tudarmstadt/ukp/similarity/dkpro/io/ShortAnswerGradingReader.java
@@ -4,13 +4,21 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.CASException;
+import org.apache.uima.collection.CollectionException;
+import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.uimafit.descriptor.ConfigurationParameter;
 
+import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
+import de.tudarmstadt.ukp.similarity.dkpro.api.type.GoldTextSimilarityScore;
 import de.tudarmstadt.ukp.similarity.dkpro.io.util.CombinationPair;
 
 public class ShortAnswerGradingReader
@@ -23,6 +31,8 @@ public class ShortAnswerGradingReader
 	public static final String PARAM_ENCODING = "Encoding";
 	@ConfigurationParameter(name=PARAM_ENCODING, mandatory=false, defaultValue="UTF-8")
 	private String encoding;
+
+    private Map<String, Double> goldScoreMap = new HashMap<String, Double>();
 
 	@Override
 	public List<CombinationPair> getAlignedPairs()
@@ -72,16 +82,22 @@ public class ShortAnswerGradingReader
 						String answer = lineSplit[2];
 						int studentID = Integer.parseInt(lineSplit[1].substring(1, lineSplit[1].length() - 1));		// Ignore surrounding braces
 						
+						String id1 = assignment + ":" + questionIndex;
+						String id2 = assignment + ":" + questionIndex + ":" + studentID;
+						
 						// Add to combination pairs
 						CombinationPair pair = new CombinationPair(inputDir.toString());
-						pair.setID1(assignment + ":" + questionIndex);
-						pair.setID2(assignment + ":" + questionIndex + ":" + studentID);
+						pair.setID1(id1);
+						pair.setID2(id2);
 						pair.setText1(refAnswer);
 						pair.setText2(answer);
+						
 						
 						System.out.println(pair.getID2() + " # " + refAnswer + " # " + answer);
 						
 						pairs.add(pair);
+						
+						goldScoreMap.put(id2, Double.parseDouble(lineSplit[0]));
 					}
 				}			
 			}
@@ -90,6 +106,28 @@ public class ShortAnswerGradingReader
 			throw new ResourceInitializationException(e);
 		}
 		
+		
+		
 		return pairs;
 	}
+
+    @Override
+    public void getNext(CAS cas)
+        throws IOException, CollectionException
+    {
+        super.getNext(cas);
+
+        try {
+            JCas jcas = cas.getJCas();
+            JCas view2 = jcas.getView(VIEW_2);
+            DocumentMetaData md2 = DocumentMetaData.get(view2);
+            
+            GoldTextSimilarityScore goldScore = new GoldTextSimilarityScore(jcas);
+            goldScore.setScore(goldScoreMap.get(md2.getDocumentId()));
+            goldScore.addToIndexes();
+        }
+        catch (CASException e) {
+            throw new CollectionException(e);
+        }
+    }
 }
