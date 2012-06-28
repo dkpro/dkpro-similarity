d82a2be01db7edacdafffaa4fe05f7f3bc9a8b05
Torsten Zesch <zesch@users.noreply.github.com>
2012-06-28 16:40:40 +0000

    Added resource for GreedyStringTiling.

---
+++ de.tudarmstadt.ukp.similarity.dkpro.core-asl/src/main/java/de/tudarmstadt/ukp/similarity/dkpro/resource/lexical/string/GreedyStringTilingMeasureResource.java
@@ -0,0 +1,32 @@
+package de.tudarmstadt.ukp.similarity.dkpro.resource.lexical.string;
+
+import java.util.Map;
+
+import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.resource.ResourceSpecifier;
+import org.uimafit.descriptor.ConfigurationParameter;
+
+import de.tudarmstadt.ukp.similarity.algorithms.lexical.string.GreedyStringTiling;
+import de.tudarmstadt.ukp.similarity.dkpro.resource.TextSimilarityResourceBase;
+
+public class GreedyStringTilingMeasureResource
+	extends TextSimilarityResourceBase
+{
+	public static final String PARAM_MIN_MATCH_LENGTH = "MinMatchLength";
+	@ConfigurationParameter(name=PARAM_MIN_MATCH_LENGTH, mandatory=true)
+	private int minMatchLength;
+	
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean initialize(ResourceSpecifier aSpecifier, Map aAdditionalParams)
+        throws ResourceInitializationException
+    {
+        if (!super.initialize(aSpecifier, aAdditionalParams)) {
+            return false;
+        }
+        
+		measure = new GreedyStringTiling(minMatchLength);
+        
+        return true;
+    }
+}
