3ceda061aa4c09331ec6ff64ba146692c663f402
baer@ukp.informatik.tu-darmstadt.de <baer@ukp.informatik.tu-darmstadt.de@ac01e6db-ab4e-4c12-cdae-32caa6b7cbd2>
2012-10-26 13:54:41 +0000

    Added some SemEval-13 code

--- trunk/de.tudarmstadt.ukp.similarity.semeval2013/src/main/java/de/tudarmstadt/ukp/similarity/semeval2013/Evaluator.java
+++ trunk/de.tudarmstadt.ukp.similarity.semeval2013/src/main/java/de/tudarmstadt/ukp/similarity/semeval2013/Evaluator.java
@@ -8,7 +8,9 @@ import static org.uimafit.factory.ExternalResourceFactory.createExternalResource
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.math.stat.correlation.PearsonsCorrelation;
@@ -20,6 +22,19 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.uimafit.factory.AggregateBuilder;
 import org.uimafit.pipeline.SimplePipeline;
+
+import weka.classifiers.AbstractClassifier;
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.classifiers.functions.LinearRegression;
+import weka.classifiers.meta.FilteredClassifier;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.filters.Filter;
+import weka.filters.supervised.attribute.AddClassification;
+import weka.filters.unsupervised.attribute.AddID;
+import weka.filters.unsupervised.attribute.Remove;
 
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Document;
 import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
@@ -40,8 +55,10 @@ import static de.tudarmstadt.ukp.similarity.semeval2013.SemEval2013Baseline.MODE
 import static de.tudarmstadt.ukp.similarity.semeval2013.SemEval2013Baseline.REPORT_FILE;
 
 
-public class Evaluation
+public class Evaluator
 {
+	public static final String LF = System.getProperty("line.separator");
+	
 	public static void runLinearRegression(Dataset train, Dataset... test)
 		throws UIMAException, IOException
 	{
@@ -77,9 +94,98 @@ public class Evaluation
 		}
 	}
 	
-	public static void runLinearRegressionCV(Dataset dataset)
+	public static void runLinearRegressionCV(Mode mode, Dataset... datasets)
+		throws Exception
 	{
+		for (Dataset dataset : datasets)
+		{
+			// Set parameters
+			int folds = 10;
+			Classifier baseClassifier = new LinearRegression();
+			
+			// Set up the random number generator
+	    	long seed = new Date().getTime();			
+			Random random = new Random(seed);	
+	    	
+			// Add IDs to the instances
+			AddID.main(new String[] {"-i", MODELS_DIR + "/" + mode.toString().toLowerCase() + "/" + dataset.toString() + ".arff",
+ 								 	 "-o", MODELS_DIR + "/" + mode.toString().toLowerCase() + "/" + dataset.toString() + "-plusIDs.arff" });
+			Instances data = DataSource.read(MODELS_DIR + "/" + mode.toString().toLowerCase() + "/" + dataset.toString() + "-plusIDs.arff");
+			data.setClassIndex(data.numAttributes() - 1);				
+			
+	        // Instantiate the Remove filter
+	        Remove removeIDFilter = new Remove();
+        	removeIDFilter.setAttributeIndices("first");
+			
+			// Randomize the data
+			data.randomize(random);
 		
+			// Perform cross-validation
+		    Instances predictedData = null;
+		    Evaluation eval = new Evaluation(data);
+		    
+		    for (int n = 0; n < folds; n++)
+		    {
+		    	Instances train = data.trainCV(folds, n, random);
+		        Instances test = data.testCV(folds, n);
+		        
+		        // Copy the classifier
+		        Classifier classifier = AbstractClassifier.makeCopy(baseClassifier);
+		        	         		        
+		        // Instantiate the FilteredClassifier
+		        FilteredClassifier filteredClassifier = new FilteredClassifier();
+		        filteredClassifier.setFilter(removeIDFilter);
+		        filteredClassifier.setClassifier(classifier);
+		        	 
+		        // Build the classifier
+		        filteredClassifier.buildClassifier(train);
+		         
+		        // Evaluate
+		        eval.evaluateModel(classifier, test);
+		        
+		        // Add predictions
+		        AddClassification filter = new AddClassification();
+		        filter.setClassifier(classifier);
+		        filter.setOutputClassification(true);
+		        filter.setOutputDistribution(false);
+		        filter.setOutputErrorFlag(true);
+		        filter.setInputFormat(train);
+		        Filter.useFilter(train, filter);  // trains the classifier
+		        
+		        Instances pred = Filter.useFilter(test, filter);  // performs predictions on test set
+		        if (predictedData == null)
+		        	predictedData = new Instances(pred, 0);
+		        for (int j = 0; j < pred.numInstances(); j++)
+		        	predictedData.add(pred.instance(j));		        
+		    }
+		    
+		    // Prepare output scores
+		    double[] scores = new double[predictedData.numInstances()];
+		    
+		    for (Instance predInst : predictedData)
+		    {
+		    	int id = new Double(predInst.value(predInst.attribute(0))).intValue() - 1;
+		    	
+		    	int valueIdx = predictedData.numAttributes() - 2;
+		    	
+		    	double value = predInst.value(predInst.attribute(valueIdx));
+		    	
+		    	scores[id] = value;
+		    	
+		    	// Limit to interval [0;5]
+				if (scores[id] > 5.0) 	scores[id] = 5.0;
+				if (scores[id] < 0.0)	scores[id] = 0.0;
+		    }
+		    
+		    // Output
+		    StringBuilder sb = new StringBuilder();
+		    for (Double score : scores)
+		    	sb.append(score.toString() + LF);
+		    
+		    FileUtils.writeStringToFile(
+		    	new File(OUTPUT_DIR + "/" + mode.toString().toLowerCase() + "/" + dataset.toString() + ".csv"),
+		    	sb.toString());
+		}
 	}
 	
 	public static void runEvaluationMetrics(Mode mode, EvaluationMetric... metrics)
