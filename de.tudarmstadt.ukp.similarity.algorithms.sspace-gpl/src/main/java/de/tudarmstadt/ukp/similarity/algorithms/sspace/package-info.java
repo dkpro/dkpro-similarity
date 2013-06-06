e2b1ea32cc48679ec6f73ed3a99e999b02f97365
baer@ukp.informatik.tu-darmstadt.de <danielbaer (daniel@nudgenudge.eu)>
2013-06-06 10:55:01 +0000

    Added package-infos

---
+++ de.tudarmstadt.ukp.similarity.algorithms.sspace-gpl/src/main/java/de/tudarmstadt/ukp/similarity/algorithms/sspace/package-info.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright 2013
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ ******************************************************************************/
+
+/**
+ * Contains text similarity algorithms that are based the S-Space Package:
+ * 
+ * David Jurgens and Keith Stevens. 2010. The S-Space Package: An Open Source
+ * Package for Word Space Models. In Proceedings of the ACL 2010 System
+ * Demonstrations, pages 30--35, Uppsala, Sweden. 
+ */
+package de.tudarmstadt.ukp.similarity.algorithms.sspace;
