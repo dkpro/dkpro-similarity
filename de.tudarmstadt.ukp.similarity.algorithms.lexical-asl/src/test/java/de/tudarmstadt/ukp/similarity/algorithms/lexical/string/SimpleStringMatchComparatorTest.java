f1f59e4ce558ff3502e9d7a04c14623708b59498
baer@ukp.informatik.tu-darmstadt.de <danielbaer (daniel@nudgenudge.eu)>
2013-06-13 13:33:39 +0000

    Added and updated test cases

--- de.tudarmstadt.ukp.similarity.algorithms.lexical-asl/src/test/java/de/tudarmstadt/ukp/similarity/algorithms/lexical/string/SimpleStringMatchComparatorTest.java
+++ de.tudarmstadt.ukp.similarity.algorithms.lexical-asl/src/test/java/de/tudarmstadt/ukp/similarity/algorithms/lexical/string/SimpleStringMatchComparatorTest.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *******************************************************************************/
-package de.tudarmstadt.ukp.similarity.algorithms.lexical;
+package de.tudarmstadt.ukp.similarity.algorithms.lexical.string;
 
 import static org.junit.Assert.assertEquals;
 
