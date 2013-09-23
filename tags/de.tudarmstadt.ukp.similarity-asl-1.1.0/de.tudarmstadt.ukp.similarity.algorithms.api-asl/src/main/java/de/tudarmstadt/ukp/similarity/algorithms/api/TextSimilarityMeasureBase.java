24f1565c8802228b1675fc22b129986236e0a558
torsten.zesch@gmail.com <torsten.zesch@gmail.com@ac01e6db-ab4e-4c12-cdae-32caa6b7cbd2>
2013-09-23 09:54:55 +0000

    [maven-release-plugin]  copy for tag de.tudarmstadt.ukp.similarity-asl-1.1.0

---
+++ tags/de.tudarmstadt.ukp.similarity-asl-1.1.0/de.tudarmstadt.ukp.similarity.algorithms.api-asl/src/main/java/de/tudarmstadt/ukp/similarity/algorithms/api/TextSimilarityMeasureBase.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright 2013
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package de.tudarmstadt.ukp.similarity.algorithms.api;
+
+import static java.util.Arrays.asList;
+
+public abstract class TextSimilarityMeasureBase
+	extends TermSimilarityMeasureBase
+	implements TextSimilarityMeasure
+{
+	@Override
+	public double getSimilarity(final String aTerm1, final String aTerm2)
+		throws SimilarityException
+	{
+		return getSimilarity(asList(aTerm1), asList(aTerm2));
+	}
+}
