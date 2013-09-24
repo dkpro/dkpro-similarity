c7e4b65542c05f573e4f6cb397e763f8340abdb1
torsten.zesch <torsten.zesch@ac01e6db-ab4e-4c12-cdae-32caa6b7cbd2>
2013-09-24 10:17:15 +0000

    Renaming packages.

--- trunk/dkpro.similarity.experiments.wordpairs-asl/src/main/java/dkpro/similarity/experiments/wordpairs/io/WordPairReader.java
+++ trunk/dkpro.similarity.experiments.wordpairs-asl/src/main/java/dkpro/similarity/experiments/wordpairs/io/WordPairReader.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
-package de.tudarmstadt.ukp.similarity.experiments.wordpairs.io;
+package dkpro.similarity.experiments.wordpairs.io;
 
 import java.io.IOException;
 
