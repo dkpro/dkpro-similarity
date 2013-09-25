25cbf96a067b03bbe8f1cef089442099e354ec4f
Torsten Zesch <zesch@users.noreply.github.com>
2013-09-25 08:43:02 +0000

    Copying tag

---
+++ de.tudarmstadt.ukp.similarity.dkpro.core-asl/src/main/java/de/tudarmstadt/ukp/similarity/dkpro/resource/structure/TokenPairOrderingResource.java
@@ -0,0 +1,35 @@
+package de.tudarmstadt.ukp.similarity.dkpro.resource.structure;
+
+import java.util.Map;
+
+import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.resource.ResourceSpecifier;
+
+import de.tudarmstadt.ukp.similarity.algorithms.structure.TokenPairOrderingMeasure;
+import de.tudarmstadt.ukp.similarity.dkpro.resource.TextSimilarityResourceBase;
+
+
+public class TokenPairOrderingResource
+	extends TextSimilarityResourceBase
+{
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @Override
+    public boolean initialize(ResourceSpecifier aSpecifier, Map aAdditionalParams)
+        throws ResourceInitializationException
+    {
+        if (!super.initialize(aSpecifier, aAdditionalParams)) {
+            return false;
+        }
+
+        this.mode = TextSimilarityResourceMode.list;
+        
+        try {
+            measure = new TokenPairOrderingMeasure();
+        }
+        catch (NumberFormatException e) {
+            throw new ResourceInitializationException(e);
+        }
+        
+        return true;
+    }
+}
