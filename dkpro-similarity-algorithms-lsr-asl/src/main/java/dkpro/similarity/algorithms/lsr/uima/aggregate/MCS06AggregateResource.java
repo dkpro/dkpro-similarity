8e69643341bd75085fa577074a3545621c8216c7
Richard Eckart de Castilho <richard.eckart@gmail.com>
2016-03-26 17:33:54 +0100

    #55 - Clean up POMs and projects - Reactivate deactivated projects - Fix several license headers - Add missing LICENSE.txt file

--- dkpro-similarity-algorithms-lsr-asl/src/main/java/dkpro/similarity/algorithms/lsr/uima/aggregate/MCS06AggregateResource.java
+++ dkpro-similarity-algorithms-lsr-asl/src/main/java/dkpro/similarity/algorithms/lsr/uima/aggregate/MCS06AggregateResource.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Copyright 2012
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package dkpro.similarity.algorithms.lsr.uima.aggregate;
 
 import java.io.File;
@@ -12,7 +29,6 @@ import org.apache.uima.resource.ResourceSpecifier;
 import dkpro.similarity.algorithms.api.TextSimilarityMeasure;
 import dkpro.similarity.algorithms.lsr.aggregate.MCS06AggregateComparator;
 import dkpro.similarity.uima.resource.TextSimilarityResourceBase;
-
 
 public class MCS06AggregateResource
 	extends TextSimilarityResourceBase
