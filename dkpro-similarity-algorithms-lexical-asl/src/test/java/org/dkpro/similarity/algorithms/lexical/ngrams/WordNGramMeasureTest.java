3c443c8eec5e143311c4afa01c48c02220fb4ff0
Richard Eckart de Castilho <richard.eckart@gmail.com>
2016-03-27 11:37:43 +0200

    No issue. Simplify code for unit test.

--- dkpro-similarity-algorithms-lexical-asl/src/test/java/org/dkpro/similarity/algorithms/lexical/ngrams/WordNGramMeasureTest.java
+++ dkpro-similarity-algorithms-lexical-asl/src/test/java/org/dkpro/similarity/algorithms/lexical/ngrams/WordNGramMeasureTest.java
@@ -17,19 +17,15 @@
  *******************************************************************************/
 package org.dkpro.similarity.algorithms.lexical.ngrams;
 
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import org.dkpro.similarity.algorithms.api.TextSimilarityMeasure;
 import org.dkpro.similarity.algorithms.lexical.ngrams.WordNGramContainmentMeasure;
 import org.dkpro.similarity.algorithms.lexical.ngrams.WordNGramJaccardMeasure;
 import org.junit.Test;
    
-
 public class WordNGramMeasureTest
 {
 	private static final double epsilon = 0.001;
@@ -38,89 +34,23 @@ public class WordNGramMeasureTest
     public void jaccardTest()
 		throws Exception
 	{
-    	Map<String,Double> idfValues = new HashMap<String,Double>();
-    	idfValues.put("the", 0.01);
-    	idfValues.put("ick", 0.3);
-    	idfValues.put("fox", 2.5);
-    	idfValues.put("dog", 2.1);
-    	idfValues.put("laz", 0.28);
-    	idfValues.put("azy", 0.27);
-    	
-		List<String> l1 = new ArrayList<String>();
-		l1.add("The");
-		l1.add("quick");
-		l1.add("brown");
-		l1.add("fox");
-		l1.add("jumps");
-		l1.add("over");
-		l1.add("the");
-		l1.add("lazy");
-		l1.add("dog");
-		
-		List<String> l2 = new ArrayList<String>();
-		l2.add("The");
-		l2.add("quick");
-		l2.add("brown");
-		l2.add("fox");
-		l2.add("jumps");
-		l2.add("over");
-		l2.add("the");
-		l2.add("lazy");
-		l2.add("dog");
-		
-		List<String> l3 = new ArrayList<String>();
-		l3.add("The");
-		l3.add("quick");
-		l3.add("brown");
-		l3.add("dog");
-		l3.add("jumps");
+        List<String> l1 = asList("The quick brown fox jumps over the lazy dog".split(" "));
+        List<String> l2 = asList("The quick brown fox jumps over the lazy dog".split(" "));
+        List<String> l3 = asList("The quick brown dog jumps".split(" "));
 
 		TextSimilarityMeasure measure = new WordNGramJaccardMeasure(3);
 
 		assertEquals(1.0, measure.getSimilarity(l1, l2), epsilon);
 		assertEquals(0.111, measure.getSimilarity(l1, l3), epsilon);
-   }
-    
+	}
+
     @Test
     public void containmentTest()
 		throws Exception
 	{
-    	Map<String,Double> idfValues = new HashMap<String,Double>();
-    	idfValues.put("the", 0.01);
-    	idfValues.put("ick", 0.3);
-    	idfValues.put("fox", 2.5);
-    	idfValues.put("dog", 2.1);
-    	idfValues.put("laz", 0.28);
-    	idfValues.put("azy", 0.27);
-    	
-		List<String> l1 = new ArrayList<String>();
-		l1.add("The");
-		l1.add("quick");
-		l1.add("brown");
-		l1.add("fox");
-		l1.add("jumps");
-		l1.add("over");
-		l1.add("the");
-		l1.add("lazy");
-		l1.add("dog");
-		
-		List<String> l2 = new ArrayList<String>();
-		l2.add("The");
-		l2.add("quick");
-		l2.add("brown");
-		l2.add("fox");
-		l2.add("jumps");
-		l2.add("over");
-		l2.add("the");
-		l2.add("lazy");
-		l2.add("dog");
-		
-		List<String> l3 = new ArrayList<String>();
-		l3.add("The");
-		l3.add("quick");
-		l3.add("brown");
-		l3.add("dog");
-		l3.add("jumps");
+        List<String> l1 = asList("The quick brown fox jumps over the lazy dog".split(" "));
+        List<String> l2 = asList("The quick brown fox jumps over the lazy dog".split(" "));
+        List<String> l3 = asList("The quick brown dog jumps".split(" "));
 
 		TextSimilarityMeasure measure = new WordNGramContainmentMeasure(3);
 
